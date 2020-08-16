13d634f0d4e89955f13d902a2bb399b97731e682
HanYu <seu_yuhan@163.com>
2020-08-16 16:15:57 +0800

    init

---
+++ src/main/java/com/yuhan/learn/project_seckill/ProjectSeckillApplication.java
@@ -0,0 +1,13 @@
+package com.yuhan.learn.project_seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSeckillApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yuhan/learn/project_seckill/ProjectSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuhan.learn.project_seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
